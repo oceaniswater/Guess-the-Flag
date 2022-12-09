12929efa89d8bf37d5d88b50822f5393dce57d5b
markgolubev <markflyingbear@gmail.com>
2022-12-09 12:11:22 +0000

    Initial Commit

---
+++ Guess the Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Марк Голубев on 09.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
